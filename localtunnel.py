e5780ae97d5aecfc59b9a2916022f6e976d2f9f7
alanwr <93482893+alanwr@users.noreply.github.com>
2023-02-02 13:22:02 -0300

    Update localtunnel.py

--- localtunnel.py
+++ localtunnel.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.sidebar.image('/APP_MC/01 Logo_TO.png')
+st.sidebar.image('/content/APP_MC/01 Logo_TO.png')
 
 st.title("Análise estatística")
 
@@ -36,7 +36,7 @@ if st.sidebar.button("EXECUTAR"):
         df = pd.read_excel('/APP_MC/gasparini.xlsx')
         amostra = int(df.shape[0] * 0.15)
     elif opcao_selecionada == "Canal de Keltner":
-        df = pd.read_excel('/APP_MC/D Canal de Keltner.xlsx')
+        df = pd.read_excel('/content/APP_MC/D Canal de Keltner.xlsx')
         amostra = int(df.shape[0] * 0.15)
     elif opcao_selecionada == "Cão Farejador":
         df = pd.read_excel('C:/Users/alan/trade/APP_MC/D Cao Farejador.xlsx')
